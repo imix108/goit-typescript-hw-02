1658866f1b0e39b5ddbf9f53d7b0c223556487f9
imix108 <136431946+imix108@users.noreply.github.com>
2024-02-13 11:12:31 +0300

    Base type/6
    Update 6.ts

--- src/baseTypes/6.ts
+++ src/baseTypes/6.ts
@@ -2,17 +2,16 @@
   Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
 */
 
-function showMessage(message: any) {
+function showMessage(message: string): void {
   console.log(message);
 }
 
-
-function calc(num1: number, num2: number):number {
+function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
 
-function customError():never {
-  throw new Error('Error');
+function customError(): never {
+  throw new Error("Error");
 }
 
 export {};
